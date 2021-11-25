cd11137e927da9a059984e371b1eea8da6be4996
Kegen Guyll <guyllkegen@gmail.com>
2021-11-24 23:25:21 -0600

    created where to watch component

---
+++ components/whereToWatch.tsx
@@ -0,0 +1,92 @@
+import { FunctionComponent, useEffect, useState } from 'react';
+import Typography from './typography';
+import Image from 'next/image';
+
+interface Props {
+  provider:
+    | 'fandango'
+    | 'vudu'
+    | 'amazon-prime-video-us'
+    | 'hbo-max'
+    | 'itunes'
+    | 'netflix'
+    | 'hulu'
+    | 'disney-plus-us';
+  availability: string;
+}
+
+const WhereToWatch: FunctionComponent<Props> = ({
+  provider,
+  availability,
+}: Props) => {
+  const [image, setImage] = useState<string>('');
+
+  useEffect(() => {
+    switch (provider) {
+      case 'fandango':
+        setImage(
+          'https://www.rottentomatoes.com/assets/pizza-pie/images/affiliates/fandango.7e22d1f94b5.svg'
+        );
+        break;
+      case 'amazon-prime-video-us':
+        setImage(
+          'https://www.rottentomatoes.com/assets/pizza-pie/images/affiliates/amazon_prime_video.aea370299cd.svg'
+        );
+        break;
+      case 'hbo-max':
+        setImage(
+          'https://www.rottentomatoes.com/assets/pizza-pie/images/affiliates/hbo_max.2426e881c8f.svg'
+        );
+        break;
+      case 'itunes':
+        setImage(
+          'https://www.rottentomatoes.com/assets/pizza-pie/images/affiliates/apple_tv.da7a28db8f5.svg'
+        );
+        break;
+      case 'netflix':
+        setImage(
+          'https://www.rottentomatoes.com/assets/pizza-pie/images/affiliates/netflix.62f265e4a21.svg'
+        );
+        break;
+      case 'hulu':
+        setImage(
+          'https://www.rottentomatoes.com/assets/pizza-pie/images/affiliates/hulu.3bc560ceca6.svg'
+        );
+        break;
+      case 'vudu':
+        setImage(
+          'https://www.rottentomatoes.com/assets/pizza-pie/images/affiliates/vudu.7b6da49f202.svg'
+        );
+        break;
+      case 'disney-plus-us':
+        setImage(
+          'https://www.rottentomatoes.com/assets/pizza-pie/images/affiliates/disney_plus.38158a14466.svg'
+        );
+        break;
+      default:
+        setImage('');
+        break;
+    }
+  }, [provider]);
+
+  if (!image) return null;
+
+  return (
+    <div className='flex space-x-5 items-center'>
+      <div className='w-20 h-10 flex items-center'>
+        <img
+          width={'inherit'}
+          height={'inherit'}
+          style={{ width: 'inherit', maxHeight: 'inherit', height: 'inherit' }}
+          className='object-contain object-left-center'
+          alt={provider}
+          src={image}
+        />
+      </div>
+
+      <Typography>{availability}</Typography>
+    </div>
+  );
+};
+
+export default WhereToWatch;
