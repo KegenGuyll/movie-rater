d583cf0e0234fab76e696136f09d20813848367d
Kegen Guyll <guyllkegen@gmail.com>
2021-12-14 00:18:47 -0600

    added new page

---
+++ pages/my-movies.tsx
@@ -0,0 +1,35 @@
+import { NextPage } from 'next';
+import { useEffect, useState } from 'react';
+import getAllReviewedMovies from '../endpoints/getAllReviewsMovie';
+import { useAuth } from '../context/AuthUserContext';
+import { MovieDocument } from '../models/firestore';
+import ReviewedMoviesTable from '../components/my-movies/ReviewedMoviesTable';
+
+const MyMovie: NextPage = () => {
+  const { authUser } = useAuth();
+  const [movies, setMovies] = useState<MovieDocument[] | null>(null);
+
+  const fetchMovies = async () => {
+    if (!authUser) return;
+
+    const token = await authUser.getIdToken();
+
+    const { res } = await getAllReviewedMovies(token);
+
+    if (res) {
+      setMovies(res.data);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, [authUser]);
+
+  return (
+    <div className='p-5'>
+      <ReviewedMoviesTable movies={movies} fetchMovies={fetchMovies} />
+    </div>
+  );
+};
+
+export default MyMovie;
