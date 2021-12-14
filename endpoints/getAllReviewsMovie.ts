e0f6fcbe7c0c600853f85b8af7704ceae808b85a
Kegen Guyll <guyllkegen@gmail.com>
2021-12-14 00:16:56 -0600

    added new endpoints

---
+++ endpoints/getAllReviewsMovie.ts
@@ -0,0 +1,27 @@
+import { AxiosResponse } from 'axios';
+import { MovieDocument } from '../models/firestore';
+
+import resolve from './resolver';
+
+interface IResponse extends AxiosResponse {
+  data: MovieDocument[];
+}
+
+interface IResolvedPopular {
+  res: IResponse | null;
+  err: Error | null;
+}
+
+export default function getAllReviewedMovies(
+  authToken: string
+): Promise<IResolvedPopular> {
+  return resolve(
+    {
+      headers: {
+        Authorization: authToken,
+      },
+      url: `/movie/all`,
+    },
+    true
+  );
+}
